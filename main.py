34ef2ec3f09762aa2637690b17f48a47d2c00f06
Pliszka <karoliniakkjk@gmail.com>
2022-01-25 22:47:08 +0100

    snake body created

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('Snake')
 
-
-
+snake= []
+for turtle in range(3):
+    Andy = Turtle(shape='square')
+    Andy.color('white')
+    Andy.penup()
+    Andy.setposition(x=0-turtle*20, y=0)
+    snake.append(Andy)
 
 
 
