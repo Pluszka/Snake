82415b785b3f059d572b3f6a3e926da47e843875
Pliszka <karoliniakkjk@gmail.com>
2022-01-25 23:26:32 +0100

    Better animaton (pieces collapse to each other )

--- main.py
+++ main.py
@@ -23,12 +23,13 @@ start = make_snake()
 while game_on:
     screen.update()
     time.sleep(0.1)
-    for sq in start:
-        sq.forward(20)
-
-
-
-
+    for sq in range(len(start) - 1, 0,-1):
+        new_x = start[sq-1].xcor()
+        new_y = start[sq - 1].ycor()
+        start[sq].setposition(new_x, new_y)
+    first_sqx = start[0].xcor()
+    first_sqy = start[0].ycor()
+    start[0].setposition(first_sqx+20, first_sqy)
 
 
 
