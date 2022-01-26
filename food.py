a06fb92c4904dfd7c8a48ff07db9ac2be9f4701a
Pliszka <karoliniakkjk@gmail.com>
2022-01-26 20:37:21 +0100

    Food created

--- food.py
+++ food.py
@@ -1,6 +1,16 @@
 from turtle import Turtle
+import random
+
 
 class Food(Turtle):
 
     def __init__(self):
         super().__init__()
+        self.shape('turtle')
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color('green')
+        rand_x = random.randint(-280, 280)
+        rand_y = random.randint(-280, 280)
+        self.setposition(x=rand_x, y=rand_y)
+
--- snake.py
+++ snake.py
@@ -6,6 +6,7 @@ DOWN = 270
 RIGHT = 0
 LEFT = 180
 
+
 class Snake:
 
     def __init__(self):
@@ -15,12 +16,11 @@ class Snake:
 
     def startSnake(self):
         for turtle in range(3):
-            Andy = Turtle(shape='square')
-            Andy.color('white')
-            Andy.penup()
-            Andy.setposition(x=0 - turtle * 20, y=0)
-            self.start_snake.append(Andy)
-
+            andy = Turtle(shape='square')
+            andy.color('white')
+            andy.penup()
+            andy.setposition(x=0 - turtle * 20, y=0)
+            self.start_snake.append(andy)
 
     def move(self):
         for sq in range(len(self.start_snake) - 1, 0, -1):
@@ -33,18 +33,14 @@ class Snake:
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
 
-
     def down(self):
         if self.head.heading() != UP:
             self.head.setheading(DOWN)
-
 
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
 
-
     def left(self):
         if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
-
